8520d16c8535d6077d0d4d22ce8d1c61f7d2b258
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2018-02-28 14:20:09 -0500

    Create StudentController class and add StubMethod method.

---
+++ AdvancedCourseProject/app/src/main/java/prodigi_development/advancedcourseproject/Controller/StudentController.java
@@ -0,0 +1,14 @@
+package prodigi_development.advancedcourseproject.Controller;
+
+/**
+ * Created by antho on 2/28/2018.
+ */
+
+public class StudentController {
+
+    public StudentController() {}
+
+    public String StubMethod() {
+        return "It's alive";
+    }
+}
