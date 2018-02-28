a704087ca5601a56a66f84fffb422ffc5ea3c9fc
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2018-02-28 14:15:34 -0500

    Move Screen Java file to View folder

--- AdvancedCourseProject/app/src/main/java/prodigi_development/advancedcourseproject/View/MainActivity.java
+++ AdvancedCourseProject/app/src/main/java/prodigi_development/advancedcourseproject/View/MainActivity.java
@@ -1,7 +1,9 @@
-package prodigi_development.advancedcourseproject;
+package prodigi_development.advancedcourseproject.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import prodigi_development.advancedcourseproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
