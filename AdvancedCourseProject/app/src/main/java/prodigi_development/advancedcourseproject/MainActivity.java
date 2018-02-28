51d5265f63f167de30459b23632a8abb2ce4680a
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2018-02-28 13:53:17 -0500

    Create new Android project

---
+++ AdvancedCourseProject/app/src/main/java/prodigi_development/advancedcourseproject/MainActivity.java
@@ -0,0 +1,13 @@
+package prodigi_development.advancedcourseproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
